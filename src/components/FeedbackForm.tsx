7c46700cda90b7a8a89a591379c56d2aea3c977a
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-11 16:01:49 +0200

    🔨 add text counter

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constant";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const charCount = MAX_CHARACTERS - text.length;
+
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="" spellCheck={false} />
+      <textarea
+        value={text}
+        onChange={(e) => {
+          const newText = e.target.value;
+          if (newText.length > MAX_CHARACTERS) {
+            return;
+          }
+          setText(newText);
+        }}
+        id="feedback-textarea"
+        placeholder=""
+        spellCheck={false}
+      />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic"> 150</p>
+        <p className="u-italic"> {charCount}</p>
         <button>
-          <span>Submit</span>{" "}
+          <span>Submit</span>
         </button>
       </div>
     </form>
