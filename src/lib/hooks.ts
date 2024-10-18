94e36ac26892cd6423b22ba4ba9f644836482f8d
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-18 12:21:24 +0200

    ♻️ custom hook for feedback items

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,6 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FeedbackItemsContext } from "../components/contexts/FeedbackItemsContextProvider";
+import { TFeedbackItem } from "./types";
 
 export function useFeedbackItemsContext() {
   const context = useContext(FeedbackItemsContext);
@@ -9,4 +10,55 @@ export function useFeedbackItemsContext() {
     );
   }
   return context;
+}
+
+export function useFeedbackItems() {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+        if (!res.ok) {
+          throw new Error("Someting went wrong.");
+        }
+        const data = await res.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage("Someting went wrong.");
+      }
+      setIsLoading(false);
+    };
+    fetchFeedbackItems();
+    // setIsLoading(true);
+    // fetch(
+    //   "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+    // )
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error("Someting went wrong.");
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setFeedbackItems(data.feedbacks);
+    //     setIsLoading(false);
+    //   })
+    //   .catch(() => {
+    //     setErrorMessage("Someting went wrong.");
+    //     setIsLoading(false);
+    //   });
+  }, []);
+
+  return {
+    feedbackItems,
+    isLoading,
+    errorMessage,
+    setFeedbackItems,
+  };
 }
