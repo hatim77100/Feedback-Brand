38036fcd33b88c8c76bb84d385112f6812c25441
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-18 11:46:11 +0200

    🐛 fix state and filter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,37 +1,16 @@
-import { useMemo, useState } from "react";
 import FeedbackItemsContextProvider from "./contexts/FeedbackItemsContextProvider";
 import HashtagList from "./hashtag/HashtagList";
 import Container from "./layout/Container";
 import Footer from "./layout/Footer";
 
 function App() {
-  const [selectedCompany, setSelectedCompany] = useState("");
-
-  const filterFeedbackItems = useMemo(
-    () =>
-      selectedCompany
-        ? feedbackItems.filter(
-            (feedbackItem) => feedbackItem.company === selectedCompany
-          )
-        : feedbackItems,
-    [selectedCompany, feedbackItems]
-  );
-
-  const handleSelectCompany = (company: string) => {
-    setSelectedCompany(company);
-  };
-
   return (
     <div className="app">
       <Footer />
-
       <FeedbackItemsContextProvider>
         <Container />
+        <HashtagList />
       </FeedbackItemsContextProvider>
-      <HashtagList
-        companyList={companyList}
-        handleSelectCompany={handleSelectCompany}
-      />
     </div>
   );
 }
