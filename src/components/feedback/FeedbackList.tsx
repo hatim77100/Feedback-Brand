4426157242f01433a359a0446d190c35a9edc37d
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-18 11:47:50 +0200

    🐛 fix select company

--- src/components/feedback/FeedbackList.tsx
+++ src/components/feedback/FeedbackList.tsx
@@ -1,16 +1,17 @@
 import ErrorMessage from "../ErrorMessage";
 import Spinner from "../Spinner";
 import FeedbackItem from "./FeedbackItem";
-import { useFeedbackItemsContext } from "../contexts/FeedbackItemsContextProvider";
+import { useFeedbackItemsContext } from "../../lib/hooks";
 
 export default function FeedbackList() {
-  const { feedbackItems, isLoading, errorMessage } = useFeedbackItemsContext();
+  const { filterFeedbackItems, isLoading, errorMessage } =
+    useFeedbackItemsContext();
 
   return (
     <ol className="feedback-list">
       {isLoading && <Spinner />}
       {errorMessage && <ErrorMessage message={errorMessage} />}
-      {feedbackItems.map((feedbackItem) => (
+      {filterFeedbackItems.map((feedbackItem) => (
         <FeedbackItem feedbackItem={feedbackItem} key={feedbackItem.id} />
       ))}
     </ol>
